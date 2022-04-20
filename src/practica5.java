21b336f5c38e327784fd3debb8a1792e973c282c
Cristhian <carce@centronelson.org>
2022-04-20 12:26:59 +0200

    añadiendo arraylist

--- src/practica5.java
+++ src/practica5.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class practica5 {
@@ -6,26 +7,21 @@ public class practica5 {
 		// TODO Auto-generated method stub
 		Scanner sn= new Scanner(System.in);
 		Scanner sc = new Scanner(System.in);
-		String nombre="";
-		String apellido="";
-		String fechaNac="";
-		String dni="";
-		String estudios="";
-		String tlf="";
+		ArrayList<String> datosUsuario = new ArrayList<String>();
 		
 		
 			System.out.println("Introduce nombre para estudiante");
-			nombre=sc.nextLine();
+			datosUsuario.add(sc.nextLine());
 			System.out.println("Introduce apellido para estudiante");
-			apellido=sc.nextLine();
+			datosUsuario.add(sc.nextLine());
 			System.out.println("Introduce fecha nacimiento para estudiante");
-			fechaNac=sc.nextLine();
+			datosUsuario.add(sc.nextLine());
 			System.out.println("Introduce dni para estudiante");
-			dni=sc.nextLine();
+			datosUsuario.add(sc.nextLine());
 			System.out.println("Introduce estudios previos para estudiante");
-			estudios=sc.nextLine();
+			datosUsuario.add(sc.nextLine());
 			System.out.println("introduzca telefono");
-			tlf=sc.nextLine();
+			datosUsuario.add(sc.nextLine());
 	}
 
 }
